4b6448ad4ee29c0a665f0f445ca1001e75889aaa
Mazuna <nicojuric@gmail.com>
2024-11-24 00:50:23 +0100

    hello-world!

---
+++ backend/src/api/hello-world/route.ts
@@ -0,0 +1,13 @@
+import type {
+    MedusaRequest,
+    MedusaResponse,
+  } from "@medusajs/framework/"
+  
+  export const GET = (
+    req: MedusaRequest,
+    res: MedusaResponse
+  ) => {
+    res.json({
+      message: "[GET] Hello world!",
+    })
+  }
